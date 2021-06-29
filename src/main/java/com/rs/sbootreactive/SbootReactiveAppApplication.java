d8351ddf4e98c42f6d7bf901c146c74947d666bf
wook32 <wook3210@gmail.com>
2021-06-29 19:11:47 +0900

    Initial commit

---
+++ src/main/java/com/rs/sbootreactive/SbootReactiveAppApplication.java
@@ -0,0 +1,13 @@
+package com.rs.sbootreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootReactiveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootReactiveAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rs/sbootreactive/SbootReactiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs.sbootreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootReactiveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
