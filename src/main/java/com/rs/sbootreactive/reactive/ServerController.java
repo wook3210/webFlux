eea4c58c2c42b5cb9b3a07a0313518023e97f7f1
wook32 <wook3210@gmail.com>
2021-06-30 17:31:05 +0900

    toby Flux create subscribe

--- src/main/java/com/rs/sbootreactive/reactive/ServerController.java
+++ src/main/java/com/rs/sbootreactive/reactive/ServerController.java
@@ -1,7 +1,12 @@
 package com.rs.sbootreactive.reactive;
 
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
@@ -22,5 +27,26 @@ public class ServerController {
     Flux<Dish> deliverDishes(){
         return this.kitchenService.getDishes()//
                 .map(dish -> Dish.deliver(dish));
+    }
+
+    @RequestMapping ("/hello/{name}")
+    Publisher<String> hello(@PathVariable String name ){
+        return new Publisher<String>() {
+            @Override
+            public void subscribe(Subscriber subscriber) {
+                subscriber.onSubscribe(new Subscription() {
+                    @Override
+                    public void request(long l) {
+                        subscriber.onNext("hello: " + name);
+                        subscriber.onComplete();
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+            }
+        };
     }
 }
